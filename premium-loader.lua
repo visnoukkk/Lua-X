26cdd04935bbcf5cadc0c0b9ea8c1602274189df
visnoukkk <chamnanchanvathana@gmail.com>
2026-09-13 10:14:54 +0700

    Update premium-loader.lua

--- premium-loader.lua
+++ premium-loader.lua
@@ -1,3 +1,11 @@
-if game.PlaceId == 17625359962 then
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/7d6f504ed4986184635bf6fea2aa11c6.lua"))()
-end
+--[[
+made by visnouk chick roaxi millad and dani
+    __                           _  __
+   / /   __  __  ____ _         | |/ /
+  / /   / / / / / __ `/         |   / 
+ / /___/ /_/ / / /_/ /         /   |  
+/_____/\__,_/  \__,_/         /_/|_| .gg/luax
+]]
+-- try to crack this or deobf this lmao
+print("loading luarmor...")
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/7d6f504ed4986184635bf6fea2aa11c6.lua"))()
